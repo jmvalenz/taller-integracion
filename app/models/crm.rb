5eb79a60bab982b045ca61272241f6b17aa13f79
José Valenzuela Demarco <jmvalenz@uc.cl>
2014-05-15 00:17:17 -0400

    Corrijo funcionalidad vtiger

--- app/models/crm.rb
+++ app/models/crm.rb
@@ -1,16 +1,23 @@
 class Crm < ActiveRecord::Base
 	
-	#include HTTParty
-  	#base_uri 'http://integra.ing.puc.cl/vtigerCRM'
-  	#default_params :output => 'json'
-  	#format :json
+	include HTTParty
+  	base_uri 'http://integra.ing.puc.cl/vtigerCRM'
+  	default_params :output => 'json'
+  	format :json
 
-  	def self.getchallenge(username)
-    	get('http://integra.ing.puc.cl/vtigerCRM/webservice.php?operation=getchallenge', :query => {:username => username})
+  	def Crm.get_challenge
+    	response = get('/webservice.php?', query: { operation: :getchallenge, username: Settings.vtiger.username })
+      JSON.parse(response.body, symbolize_names: true)
   	end
 
-  	def self.login(username, password)
-    	post('/webservice.php?operation=login', :query => {:username => username, :accessKey => password})
+  	def Crm.login
+      # Obtengo el challenge token
+      token = get_challenge[:result][:token]
+      # El API accessKey es MD5(token + accessKey)
+      generated_key = Digest::MD5.hexdigest(token.to_s + Settings.vtiger.accessKey.to_s)
+
+      response = post('/webservice.php', body: { operation: "login", username: Settings.vtiger.username, accessKey: generated_key })
+      JSON.parse(response.body, symbolize_names: true)
   	end
 
 end
