14d88ff171d9aab5bde13c2a5b6ac033dcb0dc60
Andres Riveros <andres.a.riveros@gmail.com>
2014-05-17 13:00:33 -0400

    Quito Login FTP del main
    En push anterior se movio a Order.check_new_Order

--- app/models/main.rb
+++ app/models/main.rb
@@ -14,19 +14,6 @@ class Main
     # Revisar FTP.
     # Si hay pedidos nuevos, ejecuto wholesale_process
 
-
-    Net::SFTP.start('integra.ing.puc.cl', 'grupo5', :password => '823823k') do |sftp|
-    # sftp.file.open("/home/grupo5/Pedidos/pedido_1001.xml") do |file|
-      sftp.dir.foreach("/home/grupo5/Pedidos/") do |order|
-          
-        #Se comprueba que la orden no haya sido ingresada previamente
-        if passes_validation?(remote_file)
-            Order.load_orders(order)
-            
-          order.close
-      end
-    end
-
   end
 
   def passes_validation
