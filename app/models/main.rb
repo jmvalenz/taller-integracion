676ba4e3009f72248799036d73fc8bc10ad6d689
José Valenzuela Demarco <jmvalenz@uc.cl>
2014-05-17 12:27:18 -0400

    Elimino activerecord de crm

--- app/models/main.rb
+++ app/models/main.rb
@@ -15,23 +15,23 @@ class Main
     # Si hay pedidos nuevos, ejecuto wholesale_process
 
 
-	Net::SFTP.start('integra.ing.puc.cl', 'grupo5', :password => '823823k') do |sftp|
-	# sftp.file.open("/home/grupo5/Pedidos/pedido_1001.xml") do |file|
-		sftp.dir.foreach("/home/grupo5/Pedidos/") do |order|
-	    	
-			#Se comprueba que la orden no haya sido ingresada previamente
-			if passes_validation?(remote_file)
-	    		Order.load_orders(order)
-	    		
-	    	order.close
-		end
-	end
+    Net::SFTP.start('integra.ing.puc.cl', 'grupo5', :password => '823823k') do |sftp|
+    # sftp.file.open("/home/grupo5/Pedidos/pedido_1001.xml") do |file|
+      sftp.dir.foreach("/home/grupo5/Pedidos/") do |order|
+          
+        #Se comprueba que la orden no haya sido ingresada previamente
+        if passes_validation?(remote_file)
+            Order.load_orders(order)
+            
+          order.close
+      end
+    end
 
   end
 
   def passes_validation
 
-  	return true
+    return true
 
   end
 
