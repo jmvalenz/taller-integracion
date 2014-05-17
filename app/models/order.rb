1a56cba4c31843408b0104bdc620eb237770a0bf
Andres Riveros <andres.a.riveros@gmail.com>
2014-05-17 12:58:03 -0400

    Muevo Conexion FTP y arreglo Pedidos
    Muevo la conexion al FTP a la clase Reservation y arreglo la estructuras
    de las clases Order y Product_Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,27 +5,43 @@ class Order < ActiveRecord::Base
 
     def Order.load(filename)
 	    doc = Nokogiri::XML(file)
-	    customer = Customer.find_or_create_by(rut: doc.xpath("//Pedidos//rut").to_s[/>(.*?)</, 1])
-      doc.xpath("//Pedidos//rut").to_s[/>(.*?)</, 1]
-      date = doc.xpath("//Pedidos/fecha").to_s[/>(.*?)</, 1]
-      time = doc.xpath("//Pedidos/hora").to_s[/>(.*?)</, 1]
-      date_delivery = doc.xpath("//Pedidos//fecha").to_s[/>(.*?)</, 1]
-      address_id = doc.xpath("//Pedidos//direccionId").to_s[/>(.*?)</, 1]
+	   
+      order = Order.create_by({
+        customer_id: doc.xpath("//Pedidos//rut").to_s[/>(.*?)</, 1]
+        address_id: doc.xpath("//Pedidos//direccionId").to_s[/>(.*?)</, 1]
+        date: doc.xpath("//Pedidos/fecha").to_s[/>(.*?)</, 1]
+        time: doc.xpath("//Pedidos/hora").to_s[/>(.*?)</, 1]
+        date_delivery: doc.xpath("//Pedidos//fecha").to_s[/>(.*?)</, 1]
+        
+      })
 
-      Order.load_orders(doc)
+      order.load_orders(doc)
 
       file.close
   	end
 
-  	def Order.load_orders(doc)    
-      doc.xpath("//Pedidos").each do |pedido|
-        order = Product_order.create({
-        sku: pedido.xpath("//sku").to_s[/>(.*?)</, 1], 
-        amount: pedido.xpath("//cantidad").to_s[/>(.*?)</, 1]
+  	def load_product_orders(doc)    
+      doc.xpath("//Pedidos//Pedido").each do |pedido|
+        self.product_orders.create({
+          sku: pedido.xpath("//sku").to_s[/>(.*?)</, 1], 
+          amount: pedido.xpath("//cantidad").to_s[/>(.*?)</, 1]
       })
       end
     end
   end
 
+  def Order.check_new_Order()
+    Net::SFTP.start('orders_sftp_system.url', 'orders_sftp_system.user', :password => 'orders_sftp_system.') do |sftp|
+    # sftp.file.open("/home/grupo5/Pedidos/pedido_1001.xml") do |file|
+      sftp.dir.foreach("/home/grupo5/Pedidos/") do |order|
+          
+        #Se comprueba que la orden no haya sido ingresada previamente
+        if passes_validation?(remote_file)
+            Order.load_orders(order)
+
+          order.close
+      end
+    end
+  end
 
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,6 +2,23 @@ class Reservation < ActiveRecord::Base
 
     has_many :product_reservation, dependent: :destroy
 
+
+
+  def Reservation.load_preservations(ws)
+    i = 5
+    while not ws[i,1].empty? and ws[i,4].empty?  do
+      product_reservation = Product_reservation.create({
+        sku: ws[i,1], 
+        rut: ws[i,2], 
+        amount: ws[i,3]
+      })
+      ws[i,4] = "Si"
+      i = i + 1
+    end
+    ws.save()
+  end
+
+    ##################### SYSTEM METHODS #####################
   def Product.load(key)
     key = Google::APIClient::KeyUtils.load_from_pkcs12('29238ebeddba28cc9685d3151dbff93d348c7e76-privatekey.p12', 'notasecret')
 
@@ -21,18 +38,4 @@ class Reservation < ActiveRecord::Base
     session = GoogleDrive.login_with_oauth(access_token, proxy = nil)
     ws = session.spreadsheet_by_key("0As9H3pQDLg79dEZqd1YzYl80Y0Q0TjlJZDNTclFnTUE").worksheets[0]
     self.load_preservations(ws)
-  end
-
-  def Reservation.load_preservations(ws)
-    i = 5
-    while not ws[i,1].empty? and ws[i,4].empty?  do
-      product_reservation = Product_reservation.create({
-        sku: ws[i,1], 
-        rut: ws[i,2], 
-        amount: ws[i,3]
-      })
-      ws[i,4] = "Si"
-      i = i + 1
-    end
-    ws.save()
   end
