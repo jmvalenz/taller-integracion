a039230187873b8bf1dda3523c82e299933d2548
Andres Riveros <andres.a.riveros@gmail.com>
2014-05-17 11:08:22 -0400

    Lectura de ordenes de compra
    Parseo corregido en lectura de archivos XML

--- app/models/order.rb
+++ app/models/order.rb
@@ -6,11 +6,12 @@ class Order < ActiveRecord::Base
     def Order.load(filename)
 	    file = open(filename)
 	    doc = Nokogiri::XML(file)
-	    customer = Customer.find_or_create_by(rut: doc.xpath("//Pedidos//rut"))
-      date = doc.xpath("//Pedidos[fecha]")
-      time = doc.xpath("//Pedidos[hora]")
-      date_delivery = doc.xpath("//Pedidos//fecha")
-      address_id = doc.xpath("//Pedidos//direccionId")
+	    customer = Customer.find_or_create_by(rut: doc.xpath("//Pedidos//rut").to_s[/>(.*?)</, 1])
+      doc.xpath("//Pedidos//rut").to_s[/>(.*?)</, 1]
+      date = doc.xpath("//Pedidos/fecha").to_s[/>(.*?)</, 1]
+      time = doc.xpath("//Pedidos/hora").to_s[/>(.*?)</, 1]
+      date_delivery = doc.xpath("//Pedidos//fecha").to_s[/>(.*?)</, 1]
+      address_id = doc.xpath("//Pedidos//direccionId").to_s[/>(.*?)</, 1]
 
       Order.load_orders(doc)
 
@@ -20,8 +21,8 @@ class Order < ActiveRecord::Base
   	def Order.load_orders(doc)    
       doc.xpath("//Pedidos").each do |pedido|
         order = Product_order.create({
-        sku: pedido.xpath("//sku"), 
-        amount: pedido.xpath("//cantidad")
+        sku: pedido.xpath("//sku").to_s[/>(.*?)</, 1], 
+        amount: pedido.xpath("//cantidad").to_s[/>(.*?)</, 1]
       })
       end
     end
