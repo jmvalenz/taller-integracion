0c635e4791eaf97e7b36ab9640d465f437f36223
José Valenzuela Demarco <jmvalenz@uc.cl>
2014-05-18 02:02:46 -0400

    Arreglo error en migracion

--- db/migrate/20140517191636_create_reservation.rb
+++ db/migrate/20140517191636_create_reservation.rb
@@ -1,10 +1,11 @@
 class CreateReservation < ActiveRecord::Migration
   def change
     create_table :reservation do |t|
-      t.integer   :sku
-      t.integer   :customer_id
+      t.string   :sku
+      t.string   :customer_id
       t.integer   :amount
 
       t.timestamps
+    end
   end
 end
