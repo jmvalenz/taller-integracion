9801d33c31229bf06735c160572f302f0f1c3931
frcavada <frcavada@uc.cl>
2014-05-20 02:43:14 -0400

    Métodos del customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,9 +4,21 @@ class Customer
   attr_accessor :_id, :first_name, :last_name, :street, :city, :state
 
   def full_address
-    (street + ", " + city + ", " + state).titleize
+    (street_address + ", " + city + ", " + state + ", Chile").titleize
   end
 
+  def street_address
+  	street_values = street.split
+  	street_address = ""
+
+  	street_values.each do |value|
+	  street_address += " " + value
+  	  if !!(value =~ /^[-+]?[0-9]+$/)
+  	  	break
+  	  end
+  	end
+  	street_address.strip
+  end
 
 
 end
