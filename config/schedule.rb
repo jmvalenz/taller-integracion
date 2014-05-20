f8ccf21f1caefc17c416814e0ca342600d02e280
José Valenzuela Demarco <jmvalenz@uc.cl>
2014-05-20 14:20:57 -0400

    Comento cronjobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -19,16 +19,20 @@
 
 # Learn more: http://github.com/javan/whenever
 
-every 10.minutes do
-  runner "Main.fetch_orders"
-  runner "Main.fetch_reservations"
+every 1.minute do
+  runner "Test.test.deliver"
 end
 
-every 12.hours do
-  runner "Main.fetch_prices"
-end
+# every 10.minutes do
+#   runner "Main.fetch_orders"
+#   runner "Main.fetch_reservations"
+# end
 
-every 24.hours do
-  runner "Main.wholesale_process"
-end
+# every 12.hours do
+#   runner "Main.fetch_prices"
+# end
+
+# every 24.hours do
+#   runner "Main.wholesale_process"
+# end
 
