b19972ea9803e1abed4d81a2c6d1f26dc16be1e4
José Valenzuela Demarco <jmvalenz@uc.cl>
2014-05-19 23:35:53 -0400

    Agrego comunicacion con Bodega 4

---
+++ app/models/warehouse_4.rb
@@ -0,0 +1,27 @@
+class Warehouse_4
+
+  include HTTParty
+    base_uri 'http://integra4.ing.puc.cl/api/'
+    default_params output: 'json'
+    format :json
+
+  USER = "grupo5"
+  PASS = "675af2de40aa875fb8877a7afa3a11e0989ae496"
+
+  def get_sku!(sku, amount, depot_id)
+    get_sku(sku, new_amount, depot_id)
+  end
+
+
+  def get_sku(sku, amount, depot_id)
+    response = self.class.post("/pedirProducto", body: { usuario: USER, password: PASS, almacen_id: depot_id, SKU: sku, cantidad: amount})
+    json = JSON.parse(response.body, symbolize_names: true)
+    binding.pry
+    if json[:SKU]
+      json[:cantidad]
+    else
+      0
+    end
+  end
+
+end
