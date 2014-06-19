ca7f87ff0779516bb5737a83c3aca8109f66d992
Patricio Castro <pcastro1@uc.cl>
2014-06-19 15:26:55 -0400

    cambio int por integer

--- db/migrate/20140617180315_create_msg_repos.rb
+++ db/migrate/20140617180315_create_msg_repos.rb
@@ -2,7 +2,7 @@ class CreateMsgRepos < ActiveRecord::Migration
   def change
     create_table :msg_repos do |t|
       t.string :sku
-      t.int :fecha
+      t.integer :fecha
       t.string :almacenId
       t.string :int
 
--- db/migrate/20140617180401_create_msg_oferta.rb
+++ db/migrate/20140617180401_create_msg_oferta.rb
@@ -2,9 +2,9 @@ class CreateMsgOferta < ActiveRecord::Migration
   def change
     create_table :msg_oferta do |t|
       t.string :sku
-      t.int :precio
-      t.int :inicio
-      t.int :fin
+      t.integer :precio
+      t.integer :inicio
+      t.integer :fin
 
       t.timestamps
     end
