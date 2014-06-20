4b104f9c3fb5fb9d73fc7ce75244b6dd92e7ad6c
sharon <smotles@puc.cl>
2014-06-19 22:50:09 -0400

    avance rabbit... no listo todavia

--- app/models/msg_oferta.rb
+++ app/models/msg_oferta.rb
@@ -1,2 +1,28 @@
 class MsgOferta < ActiveRecord::Base
+  require 'bunny'
+  
+  def self.read_msg
+
+    # start a communication session with the amqp server
+    # declare a queue
+    # declare default direct exchange which is bound to all queues
+    # publish a message to the exchange which then gets routed to the queue
+    # get message from the queue
+
+    # no estoy segura
+    channel = @conn.create_channel
+    cola = channel.queue('ofertas', :auto_delete => true)
+
+    channel.prefetch(1)
+
+  end
+
+  def self.connect
+    @conn = Bunny.new('amqp://kncydrxj:MzJ3mNOLFh-Vnj2_AA7LSiP8x9AkTUx7@tiger.cloudamqp.com/kncydrxj')
+    @conn.start
+  end
+
+  def self.disconnect
+    @conn.stop
+  end
 end
--- app/models/tw.rb
+++ app/models/tw.rb
@@ -12,4 +12,26 @@ class Tw < ActiveRecord::Base
     client.update(msg)
   end
 
+  def self.postOferta(sku, precio, inicio, fin)
+    product=Item.find(sku)
+
+    if(!product.internet_price)
+      internet_price=Procing.where(sku: sku).first.precio
+    elsif(product.internet_price<=0)
+      internet_price=Producto.where(sku: sku).first.precio
+    end 
+
+    fecha_inicio= Date.strptime((inicio/1000).to_s, '%s')
+    fecha_fin= Date.strptime((fin/1000).to_s, '%s')
+    hora_inicio=Time.at(inicio/1000)
+    hora_fin=Time.at(fin/1000)
+
+    brand = product.brand
+    name = product.name
+
+    msg="OFERTA DEL #{fecha_inicio} AL #{fecha_fin}! #{brand} #{name} - ANTES: $#{internet_price} | AHORA: $#{precio}"
+
+    tweet(msg)
+  end
+
 end
